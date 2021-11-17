f7871125259fa1ea1aec1d4baf2df66b6209ff2a
ESG <68476157+YookiBoy@users.noreply.github.com>
2021-11-17 22:57:46 +0100

    Add files via upload

---
+++ Uppgift.py
@@ -0,0 +1,129 @@
+# Berättelse till Kod 
+
+#----Del 1
+print("Del 1:")
+#Personer och pengar för varje person
+Person = 4
+kr = 25
+TotalK = kr * Person
+
+#Frukters kostnad och mängd
+Banan = 10
+Äpple = 20
+Apelsin = 15
+MängdAp= 2123
+Kiwi = 10
+
+#Varor i korgen och kvar
+Vagn = Äpple * 2+ + Banan * 3 + Apelsin * 1
+KostnadP = Vagn / Person
+Kvar = kr - KostnadP
+#Mer kiwi
+Kiw = Kiwi / 4
+#Mer Apelsin
+KostAp = Apelsin * MängdAp
+
+#--Uppgift 1
+print ("Om gruppen delar på kostnaden, hur mycket pengar har varje person kvar?", Kvar , "kr")
+print()
+#-- Uppgift 2
+print("Ja de har råd med att lägga till en kiwi, de har kvar:", Kvar - Kiw , "kr" )
+print()
+#--Uppgift 3
+print ("Extra Kostnad för 2123 Apelsiner till =", KostAp - TotalK ,"kr")
+
+print()
+print("Del 2:")
+
+# Del 2
+
+#---Upgift 1
+print("Uppgift 1")
+print()
+#Personer med skylldigheter:
+LisaS = 4
+JosefS = 4
+PelleS = 3+4
+SarahS = 1
+
+TotaltSkuld = LisaS + JosefS + PelleS + SarahS
+
+print("Totala skulden i gruppen var : ", TotaltSkuld, "kr")
+
+print()
+#---Uppgift 2
+print("Uppgift 2")
+print()
+# Pelle har mest skuld för att han är skyldig både Lisa och Sarah vilket är 3+4=7
+print("Mest skyldiga person : ", "Pelle")
+
+#---Uppgift 3
+print()
+# Hur mycket pengar de har kvar i gruppen (Tog variabel Kvar från Del 1 Uppgift 2)
+print("Uppgift 3")
+print()
+Lisa = Kvar - LisaS
+Pelle = Kvar - PelleS
+Josef = Kvar - JosefS
+Sarah = Kvar - SarahS
+
+print("Sarah har: ", Sarah, "kr kvar, och därför mest pengar i gruppen.")
+
+
+#------Uppgift 4
+print()
+print("Uppgift 4")
+print()
+print("Sarah har:",Sarah, "Lisa har :",Lisa, "Josef:",Josef, "Pelle har:",Pelle)
+print()
+
+# Del 3
+print()
+print("Del 3")
+print()
+
+#Uppgift 1
+
+#Variabler
+
+Spelare = 11
+RodKort = 3
+GulKort = 2
+
+#Hur många fick röda kort?
+print("Bara", RodKort, "personer fick röd kort")
+print()
+#Hur många är dem just nu?
+print("Hur många är dem just nu?", Spelare - RodKort, "Spelare")
+print()
+#Hur många fick Gul kort?
+print("Bara", GulKort, "personer fick gul kort.")
+print()
+#Hur ser tabellen ut?
+print("Spelare: ", Spelare - RodKort, "Dem som fick röd kort:", RodKort, "Dem som fick Gul kort:", GulKort)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
